eaa5633bffb193ccd2f2a8a88d36c5f8adf78088
keinesecrets1 <miethe.max@web.de>
2022-12-26 13:28:08 +0100

    Initial commit | Changed ban command: Now able to ban offline players (not ready for prod)

--- src/main/java/eu/skyphantom/skypvp/commands/BanCommand.java
+++ src/main/java/eu/skyphantom/skypvp/commands/BanCommand.java
@@ -3,27 +3,13 @@ package eu.skyphantom.skypvp.commands;
 import eu.skyphantom.skypvp.SkyPvP;
 import eu.skyphantom.skypvp.punish.BanManager;
 import eu.skyphantom.skypvp.punish.Bans;
-import eu.skyphantom.skypvp.reports.Reports;
 import eu.skyphantom.skypvp.utils.UUIDFetcher;
-import eu.skyphantom.skypvp.utils.Utils;
-import eu.skyphantom.skypvp.utils.cooldowns.CooldownManager;
-import eu.skyphantom.skypvp.utils.time.TimeUtil;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.awt.*;
-import java.text.SimpleDateFormat;
-import java.time.OffsetDateTime;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.util.UUID;
 
 public class BanCommand extends Command {
 
@@ -95,9 +81,47 @@ public class BanCommand extends Command {
                     player.sendMessage(SkyPvP.PREFIX + "§7Gebe eine gültige ID an§8.");
                     return true;
                 }
+            } else {
+                UUID uuid = UUIDFetcher.getUUID(args[0]);
+                try {
+                    BanManager banManager = new BanManager(uuid);
+                    int id = Integer.parseInt(args[1]);
+                    Bans ban = null;
+                    for (Bans bans : Bans.values()) {
+                        int banId = bans.ordinal() + 1;
+                        if (id == banId) ban = bans;
+                    }
+                    if (ban != null) {
+                        if (ban == Bans.ADMINBAN) {
+                            if (player.hasPermission("system.admin")) {
+                                banManager.ban(ban.getName(), (ban.getTime()));
+                                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                                    if (!onlinePlayer.hasPermission("system.team")) continue;
+                                    onlinePlayer.sendMessage(SkyPvP.PUNISH + "§7Der Spieler §c" + args[0] + "§7 wurde gebannt§8.");
+                                    onlinePlayer.sendMessage(SkyPvP.PUNISH + "§7Grund§8: §c" + ban.getName() + "§8 × §c" + ban.getTimeString());
+                                    onlinePlayer.sendMessage(SkyPvP.PUNISH + "§7Von§8: §c" + player.getDisplayName());
+                                }
+                                return true;
+                            }
+                            player.sendMessage(SkyPvP.NOPERM);
+                            return true;
+                        }
+                        banManager.ban(ban.getName(), (ban.getTime()));
+                        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                            if (!onlinePlayer.hasPermission("system.team")) continue;
+                            onlinePlayer.sendMessage(SkyPvP.PUNISH + "§7Der Spieler §c" + args[0] + "§7 wurde gebannt§8.");
+                            onlinePlayer.sendMessage(SkyPvP.PUNISH + "§7Grund§8: §c" + ban.getName() + "§8 × §c" + ban.getTimeString());
+                            onlinePlayer.sendMessage(SkyPvP.PUNISH + "§7Von§8: §c" + player.getDisplayName());
+                        }
+                        return true;
+                    }
+                    player.sendMessage(SkyPvP.PREFIX + "§7Ein Bangrund mit dieser ID existiert nicht§8.");
+                    return true;
+                } catch (NumberFormatException e) {
+                    player.sendMessage(SkyPvP.PREFIX + "§7Gebe eine gültige ID an§8.");
+                    return true;
+                }
             }
-            player.sendMessage(SkyPvP.PREFIX + "§7Der Spieler ist nicht online§8.");
-            return true;
         }
         return false;
     }
